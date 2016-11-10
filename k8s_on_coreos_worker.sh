0a54f89eaac97a8ee8735b12134ff8bac0485f8d
bluewings1211 <pm751211@hotmail.com>
2016-11-10 08:30:45 +0000

    fix syntax error

--- k8s_on_coreos_worker.sh
+++ k8s_on_coreos_worker.sh
@@ -4,6 +4,8 @@ COREOS_IP=$2
 echo "MASTER_IP: $1"
 echo "COREOS_IP: $2"
 
+git clone -b v0.7.1 https://github.com/coreos/coreos-kubernetes.git
+
 echo 'Edit environment varible'
 touch /etc/environment
 cat << EOF > /etc/environment
@@ -12,11 +14,9 @@ COREOS_PUBLIC_IPV4=$COREOS_IP
 EOF
 
 echo 'Edit installation script'
-sed -i "s/.*export ETCD_ENDPOINTS=.*/export ETCD_ENDPOINTS=http:\/\/$MASTER_IP:2379/g" /root/coreos-kubernetes/multi-node/generic/worker-install.sh
+sed -i "s/.*export ETCD_ENDPOINT=.*/export ETCD_ENDPOINT=http:\/\/$MASTER_IP:2379/g" /root/coreos-kubernetes/multi-node/generic/worker-install.sh
 sed -i "s/.*export CONTROLLER_ENDPOINT=.*/export CONTROLLER_ENDPOINT=https:\/\/$MASTER_IP/g" /root/coreos-kubernetes/multi-node/generic/worker-install.sh
 
-
-git clone -b 0.7.1 https://github.com/coreos/coreos-kubernetes.git
 mkdir -p /etc/kubernetes/ssl/
 
 echo "### need copy key ###"
